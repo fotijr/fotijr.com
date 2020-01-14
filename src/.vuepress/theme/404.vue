20daa85ef96cc27605a4a13b8b85a89df718e509
Dominic Foti <dominic@fotijr.com>
2020-01-13 22:47:06 -0500

    Footer on bottom even with short content

--- src/.vuepress/theme/404.vue
+++ src/.vuepress/theme/404.vue
@@ -1,27 +1,10 @@
 <template>
   <div>
     <site-header></site-header>
-    <h1>Page Not Found</h1>
-    <p>Sorry, I'm not sure what you're looking for.</p>
+    <div class="content__default">
+      <h1>Page Not Found</h1>
+      <p>Sorry, I'm not sure what you're looking for.</p>
+    </div>
     <site-footer></site-footer>
   </div>
 </template>
-
-<script>
-import SiteHeader from '../components/SiteHeader';
-import SiteFooter from '../components/SiteFooter';
-
-export default {
-  data() {
-    return { menuVisible: false };
-  },
-  components: {
-    "site-header": SiteHeader,
-    "site-footer": SiteFooter
-  },
-  methods: {}
-};
-</script>
-
-<style lang="scss">
-</style>
